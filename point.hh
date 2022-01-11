bab0a82b2368687f6d03bde675b6377f54c40690
Viclegrand7 <victor.drouard@free.fr>
2022-01-11 15:12:19 +0100

    cout

--- point.hh
+++ point.hh
@@ -5,6 +5,8 @@
 #define BORDER_NEUMANN 1
 #define BORDER_DIRICHLET 2
 
+#include <iostream>
+
 class Point {
 	double att_x;
 	double att_y;
@@ -17,6 +19,9 @@ public:
 	unsigned char borderType() {return att_borderType;}
 	long long getId() {return att_id;}
 	Point(double x, double y, unsigned char borderType = BORDER_NO_CONDITION) : att_x(x), att_y(y), att_borderType(borderType), att_id(++globalId) {}
+	friend std :: ostream &operator<<(std :: ostream &out, Point &self) {
+		return out << self.att_x << '\t' << self.att_y;
+	}
 };
 
 #endif /* BV_TD_POINT_HH */
